e5ca65a2d54fa4dcfaba15a19e102c3dab6223f1
Juan Lopez <jc.lopez11@uniandes.edu.co>
2016-11-16 13:50:14 -0500

    notes on stocastic differential equations

--- stocastic-rq.py
+++ stocastic-rq.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 
 def wiener():
-    dt = 0.01
+    dt = 0.1
     T = 1.
     w = np.array([0.])
 
@@ -12,13 +12,27 @@ def wiener():
         w = np.append(w,[w[-1] +dt**0.5*np.random.randn()])
     return w
 
-t = np.linspace(0,1.,100)
+#nntaleb
+t = np.linspace(0,1.,10)
+"""
 u_mean = np.zeros(100)
 for i in range(1000):
     u_mean += np.exp(t+0.5*wiener())/1000.
     
 plt.plot(t,u_mean)
 plt.plot(t,np.exp(9.*t/8.))
+plt.show()
+"""
+
+x = np.array([307.65])
+w = wiener()
+k = 0.75
+s = 0.3
+for i in range(1,10):
+    x = np.append(x,[x[-1]+k*x[-1]*0.1+s*x[-1]*(w[i]-w[i-1])])
+
+plt.plot(t,x)
+plt.plot(t,x[0]*np.exp((k-0.5*s**2)*t+s*w))
 plt.show()
 
 """ra = np.random.random((5000,2))
